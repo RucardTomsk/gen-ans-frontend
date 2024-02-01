d0992f2120695eb8b4c390b554d0861fa6033071
Yuraev Aleksandr <podsolnyx03@mail.ru>
2024-02-02 01:57:30 +0700

    fix

--- src/pages/unauthorized/register/hooks/useRegister.ts
+++ src/pages/unauthorized/register/hooks/useRegister.ts
@@ -2,7 +2,7 @@ import {useMutation} from "@tanstack/react-query";
 import authService from "../../../../services/auth/AuthService.ts";
 import {useAuth} from "../../../../providers/auth";
 import {AccountRegisterDto} from "../../../../services/auth/models/AccountRegisterDto.ts";
-import {generatePath, useNavigate} from "react-router-dom";
+import {useNavigate} from "react-router-dom";
 import {Links} from "../../../../constants/links.ts";
 
 export function useRegister() {
@@ -13,8 +13,8 @@ export function useRegister() {
     return useMutation( {
         mutationFn: (data: AccountRegisterDto) => authService.register(data),
         onSuccess: (data) => {
-            signIn(data.data.accessToken, data.data.refreshToken, true)
-            navigate(generatePath(Links.Authorized.Case, {caseId: "683600e2-2e90-43f5-afa9-2415615441ca"}))
+            signIn(data.data.token, data.data.refreshToken, true)
+            navigate(Links.Authorized.Projects)
         }
     });
 }
